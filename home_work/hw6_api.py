ab5aff4ee966e6daec0d39bb0591513a3c4d4265
Dima Nov <bbs.minsk@gmail.com>
2021-04-23 00:35:48 +0300

    API tests

---
+++ home_work/hw6_api.py
@@ -0,0 +1,197 @@
+import requests
+import json
+
+
+# Save image
+# url = "https://imgs.xkcd.com/comics/state_word_map.png"
+# r = requests.get(url)
+# with open("F:\\Дима\\_IT Info\\Python_tutorial\\2021-03-18-QAForEveryone\\images\\map.png", "wb") as file:
+#     file.write(r.content)
+
+class HttpbinAPI:
+
+    def get_page(self):
+        url = "https://httpbin.org/get"
+        payload = {'page': 2, 'any-key': 'any-value'}
+        r = requests.get(url, payload)
+        print(r.json())
+        assert r.json()['args']['any-key'] == 'any-value', 'Wrong assertion'
+        return self
+
+    def add_info(self):
+        url = "https://httpbin.org/post"
+        payload = {'page': 2, 'any-key': 'any-value'}
+        r = requests.post(url, payload)
+        assert r.json()['form']['any-key'] == 'any-value', 'Wrong assertion'
+        return self
+
+    def get_auth_token(self):
+        url = "https://httpbin.org/basic-auth/dima/nov"
+        r = requests.get(url, auth=('dima', 'nov'))
+        assert r.json()["authenticated"] == 1
+        return self
+
+
+class RestfulBookerAPI:
+
+    def auth_token(self):
+        payload = {
+            "username": "admin",
+            "password": "password123"
+        }
+        url = "https://restful-booker.herokuapp.com/auth"
+        r = requests.post(url, payload)
+        actual_result = r.status_code
+        expected_result = 200
+        token = r.json()["token"]
+        assert actual_result == expected_result, f"Actual result: {actual_result}, Expected: {expected_result}"
+        return token
+
+    def create_booking(self):
+        url = "https://restful-booker.herokuapp.com/booking"
+        s = requests.Session()
+        headers = {"Accept": "application/json", "Content-Type": "application/json"}
+        payload = {
+            "firstname": "Jim",
+            "lastname": "Brown",
+            "totalprice": 1234,
+            "depositpaid": True,
+            "bookingdates": {
+                "checkin": "2018-01-01",
+                "checkout": "2019-01-01"
+            },
+            "additionalneeds": "Breakfast"
+        }
+        r = s.post(url, data=json.dumps(payload), headers=headers)
+        actual_result = r.status_code
+        expected_result = 200
+        assert actual_result == expected_result, f"Actual result: {actual_result}, Expected: {expected_result}"
+        id = r.json()["bookingid"]
+        return id
+
+    def get_booking(self, id, total_price):
+        url = f"https://restful-booker.herokuapp.com/booking/{id}"
+        r = requests.get(url)
+        actual_result = r.json()["totalprice"]
+        expected_result = total_price
+        assert actual_result == expected_result, f"Actual result: {actual_result}, Expected result: {expected_result}"
+        return self
+
+
+    def update_booking(self, id, tokent):
+        url = f"https://restful-booker.herokuapp.com/booking/{id}"
+        headers = {"Accept": "application/json", "Content-Type": "application/json", "token": tokent}
+        payload = {
+            "firstname": "Jim",
+            "lastname": "Brown",
+            "totalprice": 22222,
+            "depositpaid": True,
+            "bookingdates": {
+                "checkin": "2018-01-01",
+                "checkout": "2019-01-01"
+            },
+            "additionalneeds": "Breakfast"
+        }
+        r = requests.put(url, data=json.dumps(payload), headers=headers)
+
+        # actual_result = r.json()["totalprice"]
+        # expected_result = 22222,
+        # assert actual_result == expected_result, f"Actual result: {actual_result}, Expected result: {expected_result}"
+        return r.status_code
+
+
+class ReqRes:
+
+    def __init__(self):
+        self.id = 1
+
+    def get_user(self, id):
+        url = f"https://reqres.in/api/users/{id}"
+        r = requests.get(url)
+        actual_result = r.json()["data"]["id"]
+        expected_result = id
+        assert actual_result == expected_result, f"Actual result: {actual_result}, Expected result: {expected_result}"
+        return self
+
+    def create_user(self):
+        url = f"https://reqres.in/api/users"
+        payload = {
+            "name": "Dima",
+            "job": "qa"
+        }
+        r = requests.post(url, payload)
+        id = r.json()["id"]
+        status_code = r.status_code
+        assert status_code == 201, f"Status code: {status_code}"
+        self.id = id
+        return self
+
+
+class SwaggerPet:
+
+    def create_pet(self):
+        url = "https://petstore.swagger.io/v2/pet"
+        headers = {"Content-Type": "application/json"}
+        payload = {
+          "id": 1000,
+          "category": {
+            "id": 0,
+            "name": "string1000"
+          },
+          "name": "doggie1000",
+          "photoUrls": [
+            "string"
+          ],
+          "tags": [
+            {
+              "id": 0,
+              "name": "string"
+            }
+          ],
+          "status": "available"
+        }
+        r = requests.post(url, data=json.dumps(payload), headers=headers)
+        id = r.json()["id"]
+        status_code = r.status_code
+        assert status_code == 200, f"Status code: {status_code}"
+        self.id = id
+        return self
+
+    def get_pet(self, id):
+        url = f"https://petstore.swagger.io/v2/pet/{id}"
+        r = requests.get(url)
+        actual_result = r.json()["id"]
+        expected_result = id
+        assert actual_result == expected_result, f"Actual result: {actual_result}, Expected result: {expected_result}"
+        self.pet_name = r.json()["name"]
+        return self
+
+
+    def change_name(self):
+        url = "https://petstore.swagger.io/v2/pet"
+        headers = {"Content-Type": "application/json"}
+        payload = {
+          "id": 1000,
+          "category": {
+            "id": 0,
+            "name": "string1000"
+          },
+          "name": "vesta1000",
+          "photoUrls": [
+            "string"
+          ],
+          "tags": [
+            {
+              "id": 0,
+              "name": "string"
+            }
+          ],
+          "status": "available"
+        }
+        r = requests.put(url, data=json.dumps(payload), headers=headers)
+        actual_name = r.json()["name"]
+        expected_name = "vesta1000"
+        assert actual_name == expected_name, f"actual_name: {actual_name} expected_name: {expected_name}"
+        self.pet_name = actual_name
+        return self
+
--- tests/test_account.py
+++ tests/test_account.py
@@ -1,6 +1,8 @@
 import pytest
 from data import atm_data
 from dsl.atm_check_methods import Atm
+from home_work.hw6_api import HttpbinAPI, RestfulBookerAPI, ReqRes, SwaggerPet
+
 
 
 @pytest.mark.sanity
@@ -23,3 +25,46 @@ def test_verify_contribution_amounts_are_correct():
     test = (Atm().
             verify_contribution_amounts_are_correct()
     )
+
+
+# @pytest.mark.httpbin
+def test_httpbin_API():
+    test = (HttpbinAPI().
+            get_auth_token().
+            get_page().
+            add_info()
+    )
+
+
+# @pytest.mark.api
+def test_restful_booker_API():
+    token = RestfulBookerAPI().auth_token()
+    print(token)
+    booking_id = RestfulBookerAPI().create_booking()
+    print(booking_id)
+    test = (RestfulBookerAPI().
+            get_booking(booking_id, 1234).
+            update_booking(booking_id, token)
+    )
+    print(test)
+
+
+# @pytest.mark.api
+def test_reqres_API(id=2):
+    test = (
+        ReqRes().get_user(id).create_user()
+    )
+    print(test.id)
+    # user_id = test.id
+    # test = test.get_user()
+
+
+@pytest.mark.api
+def test_swagger_pet():
+    test = SwaggerPet().create_pet()
+    id = test.id
+    test = test.get_pet(id)
+    previous_name = test.pet_name
+    test = test.change_name()
+    current_name = test.pet_name
+    assert previous_name != current_name
