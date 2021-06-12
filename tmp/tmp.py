4c29281586eeeaec2661db3ff494b5688d400226
Dima Nov <bbs.minsk@gmail.com>
2021-06-12 21:49:38 +0300

    Add subclass

---
+++ tmp/tmp.py
@@ -0,0 +1,24 @@
+class Color:
+    def __init__(self):
+        # object attributes
+        self.name = 'Default Color'
+        self.lg = self.Lightgreen()
+
+    def show(self):
+        print("Name:", self.name)
+
+    class Lightgreen:
+        def __init__(self):
+            self.name = 'Light Green'
+            self.code = '024avc'
+
+        def display(self):
+            print("Name1:", self.name)
+            print("Code1:", self.code)
+
+
+class Canvas(Color):
+    pass
+
+canvas = Canvas()
+canvas.lg.display()
